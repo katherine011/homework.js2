91b887c31f0606379bf1abb46e6b8af640eebabf
katherine011 <guluaq09@gmail.com>
2024-10-20 22:41:06 +0400

    homework.js2

---
+++ script.js
@@ -0,0 +1,54 @@
+//1)
+let x = 11
+
+let number = x * x
+    
+console.log(number)
+
+if (number > 100 ) {
+    console.log("მეტია 100-ზე");
+    
+} else {
+    console.log("ნაკლებია 100-ზე");
+    
+}
+
+//2)
+let y = 22
+
+let numb = y
+
+console.log(numb);
+
+if (100 > numb > 50 ) {
+    console.log("რიცხვი მეტია 50-ზე და ნაკლებია 100-ზე");
+} else {
+    console.log("რიცხვი არ არის 50-ზე მეტი");
+}
+
+//3)
+let x1 = 32 
+
+let numb1 = x1
+
+console.log(numb1);
+
+if (0 >= numb1 ) {
+    console.log("რიცხვი უდრის ნულს ან ნაკლებია ნულზე");
+} else {
+    console.log("რიცხვი დადებითია");
+}
+
+//4)
+let a = 13
+let b = 31
+
+let p = (a + b) * 2
+
+console.log(p);
+
+if (p > 50 ) {
+    console.log("პერიმეტრი მეტია 50-ზე");
+} else {
+    console.log("პერიმეტრი ნაკლებია 50-ზე");
+}
